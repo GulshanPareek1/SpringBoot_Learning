3776a640759e5b937be5cf938403eab2140a7781
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-10-13 14:10:46 +0530

    Update Create-Connection.java

--- JDBC/Create-Connection.java
+++ JDBC/Create-Connection.java
@@ -5,13 +5,15 @@ class FirstJDBC{
         try{
             // load the driver 
             Class.forName("com.mysql.cj.jdbc.Driver");
-
+            // here we loaded the class 
+            // but class name was Driver and package was com.mysql.jdbc so i wrote here fully qualified name 
             // creating a connection 
             String url = "jdbc:mysql://127.0.0.1:3306/JDBC1";
+            // JDBC1 is database name which i create seprate for this jdbc understanding every action will be stored here 
             String username = "root";
             String password = "Gulshan@123";
             Connection con = DriverManager.getConnection(url,username,password);
-
+            // this is getconnection method of DriverManager 
             if(con.isClosed())
             {
                 System.out.println("Connection Closed");
@@ -25,5 +27,6 @@ class FirstJDBC{
         {
             e.printStackTrace();
         }
+        // always write your code in try catch block so you don't miss any exception or error in code 
     }
 }
